e355f7ac8ebca2ef9b74f93d4c6f77cf37c1b7e9
Ondrej Sika <ondrej@ondrejsika.com>
2024-09-18 17:26:43 +0200

    feat(gitlab_update_file): Create Gitlab file updater

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/sikalabs/slr/cmd/dela"
 	_ "github.com/sikalabs/slr/cmd/example"
 	_ "github.com/sikalabs/slr/cmd/get_jwt_from_oidc"
+	_ "github.com/sikalabs/slr/cmd/gitlab_update_file"
 	_ "github.com/sikalabs/slr/cmd/redis_set_large_data"
 	"github.com/sikalabs/slr/cmd/root"
 	_ "github.com/sikalabs/slr/cmd/test_clisso_cli"
