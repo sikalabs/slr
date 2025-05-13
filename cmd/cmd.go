73821caeeaf393dd755d3a1d41d4515d905ff829
Vojtech Pastyrik <vojtech.pastyrik@wesecon.cz>
2025-05-13 19:20:22 +0200

    feat(cmd/gitlab_create_branch): Add slr gitlab-create-branch

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/sikalabs/slr/cmd/get_gps_info_from_jpg"
 	_ "github.com/sikalabs/slr/cmd/get_helm_chart_version_from_repo"
 	_ "github.com/sikalabs/slr/cmd/get_jwt_from_oidc"
+	_ "github.com/sikalabs/slr/cmd/gitlab_create_branch"
 	_ "github.com/sikalabs/slr/cmd/gitlab_create_merge_request"
 	_ "github.com/sikalabs/slr/cmd/gitlab_update_file"
 	_ "github.com/sikalabs/slr/cmd/gitlab_update_files"
