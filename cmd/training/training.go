ac52be6b1d5e79cca20c9d4fa9f0c418ef1a4efd
Ondrej Sika <ondrej@ondrejsika.com>
2026-05-19 23:24:10 +0200

    feat(cmd/training): Add slu + version to short description

--- cmd/training/training.go
+++ cmd/training/training.go
@@ -1,11 +1,12 @@
 package training
 
 import (
+	"github.com/sikalabs/slr/version"
 	"github.com/spf13/cobra"
 )
 
 var Cmd = &cobra.Command{
 	Use:     "training",
 	Aliases: []string{"tr", "t"},
-	Short:   "Space for Ondrej Sika's training related stuff",
+	Short:   "Space for Ondrej Sika's training related stuff (slr " + version.Version + ")",
 }
