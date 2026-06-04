d10c5cad84c09fcc931ef4754877d47af4312d52
Ondrej Sika <ondrej@ondrejsika.com>
2026-06-04 06:51:02 +0200

    feat(install.sh): Dont use install-slu, it is deprecated

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-VERSION=v0.1.0 && \
-OS=linux && \
-ARCH=amd64 && \
-BIN=install-slu && \
-curl -fsSL https://github.com/sikalabs/${BIN}/releases/download/${VERSION}/${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz -o ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
-tar -xzf ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz ${BIN} && \
-rm ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
-mv ${BIN} /usr/local/bin/ && \
-/usr/local/bin/install-slu install && \
+curl -fsSL https://raw.githubusercontent.com/sikalabs/slu/master/install.sh | sh
 /usr/local/bin/slu install-bin slr
